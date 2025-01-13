4b37fb8a5b22c59a2146e06c5c1ae43e5041ae1a
Nicole Burmann <daria215@gmail.com>
2025-01-13 15:42:16 +0000

    Set secret key

--- outdoorfun/settings.py
+++ outdoorfun/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3%4zy6ca9-xwu*2)!_yeuhz!z5db3e73_(sr8gy!#mm95w##&1'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
