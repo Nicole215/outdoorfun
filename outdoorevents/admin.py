1f29aa48b8a2e1dd7b77906a75f82fd6917f573f
Nicole Burmann <daria215@gmail.com>
2025-01-14 16:21:17 +0000

    Add Event model

--- outdoorevents/admin.py
+++ outdoorevents/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Organisator, Event
 
-# Register your models here.
+
+@admin.register(Organisator)
+class OrganisatorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('organisator', 'event_type', 'town', 'country', 'date', 'slug')
+    list_filter = ('event_type', 'town', 'country', 'date')
+    search_fields = ('organisator__name', 'town', 'country', 'event_type')
--- outdoorevents/models.py
+++ outdoorevents/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Organisator(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    HIKING = 'Hiking'
+    MUD_RUN = 'Mud Run'
+    OTHER = 'Other'
+
+    EVENT_TYPE_CHOICES = [
+        (HIKING, 'Hiking'),
+        (MUD_RUN, 'Mud Run'),
+        (OTHER, 'Other'),
+    ]
+
+    organisator = models.ForeignKey(Organisator, on_delete=models.CASCADE, related_name="events")
+    town = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    date = models.DateField()
+    event_type = models.CharField(max_length=20, choices=EVENT_TYPE_CHOICES, default=OTHER)
+    slug = models.SlugField(unique=True, editable=False)
+
+    def __str__(self):
+        return f"{self.organisator} - {self.date} - {self.town}"
+
+    def save(self, *args, **kwargs):
+        # Automatically generate a unique slug
+        if not self.slug:
+            self.slug = slugify(f"{self.organisator}-{self.town}-{self.date}")
+        super().save(*args, **kwargs)
