4b88a8211c28b3ad76ad3f3a9eddeb2b8831e3bc
Nicole Burmann <daria215@gmail.com>
2025-01-16 17:52:12 +0000

    Create index and base.html

--- outdoorevents/views.py
+++ outdoorevents/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Event
+from .models import Event, Organisator
 
 
 class EventList(generic.ListView):
     queryset = Event.objects.all()
-    template_name = "event_list.html"
+    template_name = "outdoorevents/index.html"
+    paginate_by = 6
